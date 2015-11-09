8a85953f9c0079035fa8c3f7d9a304f54b58beab
Kaique da Silva <kaique.developer@gmail.com>
2015-11-08 23:34:02 -0200

    Refactor on element reference

--- jankenpo.es6.js
+++ jankenpo.es6.js
@@ -1,28 +1,33 @@
 if (Meteor.isClient) {
   angular
     .module('jankenpo', ['angular-meteor', 'ionic'])
-    .factory('nextElements', ['$log', ($log) => {
+    .factory('nextElements', [
+      '$log', 'rock',
+      'paper', 'scissors', (
+      $log, rock,
+      paper, scissors
+    ) => {
       return (elements, element) => {
         $log.debug('next elements ' + elements + ' with ' + element);
         var randomPosition = Math.max(0, Math.round(Math.random() * 1));
-        if (element === 'pedra' && (randomPosition)) {
-          elements.push('tesoura');
-          elements.push('papel');
-        } else if (element === 'pedra' && (!randomPosition)) {
-          elements.push('papel');
-          elements.push('tesoura');
-        } else if (element === 'papel' && (randomPosition)) {
-          elements.push('tesoura');
-          elements.push('pedra');
-        } else if (element === 'papel' && (!randomPosition)) {
-          elements.push('tesoura');
-          elements.push('pedra');
-        } else if (element === 'tesoura' && (randomPosition)) {
-          elements.push('pedra');
-          elements.push('papel');
-        } else if (element === 'tesoura' && (!randomPosition)) {
-          elements.push('papel');
-          elements.push('pedra');
+        if (element === rock && (randomPosition)) {
+          elements.push(scissors);
+          elements.push(paper);
+        } else if (element === rock && (!randomPosition)) {
+          elements.push(paper);
+          elements.push(scissors);
+        } else if (element === paper && (randomPosition)) {
+          elements.push(scissors);
+          elements.push(rock);
+        } else if (element === paper && (!randomPosition)) {
+          elements.push(scissors);
+          elements.push(rock);
+        } else if (element === scissors && (randomPosition)) {
+          elements.push(rock);
+          elements.push(paper);
+        } else if (element === scissors && (!randomPosition)) {
+          elements.push(paper);
+          elements.push(rock);
         }
         return elements;
       };
@@ -50,15 +55,20 @@ if (Meteor.isClient) {
       rockWin: '<img ng-src="imgs/rock-win.png" class="result-img-adjusment">',
 
     })
+    .value('rock', 'pedra')
+    .value('paper', 'papel')
+    .value('scissors', 'tesoura')
     .controller('MainCtrl', [
       '$log', '$scope',
       '$ionicPopup', 'nextElements',
       'randomReverse', 'machineChoice',
-      'visualElements', (
+      'visualElements', 'rock',
+      'paper', 'scissors', (
         $log, $scope,
         $ionicPopup, nextElements,
         randomReverse, machineChoice,
-        visualElements
+        visualElements, rock,
+        paper, scissors
       ) => {
       $log.debug('O controller principal esta funcionando!');
       // scope shared vars
@@ -66,21 +76,22 @@ if (Meteor.isClient) {
       $scope.wins = 0;
       $scope.draws = 0;
       $scope.losses = 0;
+      $scope.yourChoices = [];
       // local vars
       var showAlert = (obj) => $ionicPopup.alert(obj);
       var isDraw = (elements, element, choice) => element === elements[choice];
-      var rockLoss = (elements, element, choice) => element === 'pedra' && (elements[choice] === 'papel');
-      var rockWin = (elements, element, choice) => element === 'pedra' && (elements[choice] === 'tesoura');
-      var paperLoss = (elements, element, choice) => element === 'papel' && (elements[choice] === 'tesoura');
-      var paperWin = (elements, element, choice) => element === 'papel' && (elements[choice] === 'pedra');
-      var scissorsLoss = (elements, element, choice) => element === 'tesoura' && (elements[choice] === 'pedra');
-      var scissorsWin = (elements, element, choice) => element === 'tesoura' && (elements[choice] === 'papel');
+      var rockLoss = (elements, element, choice) => element === rock && (elements[choice] === paper);
+      var rockWin = (elements, element, choice) => element === rock && (elements[choice] === scissors);
+      var paperLoss = (elements, element, choice) => element === paper && (elements[choice] === scissors);
+      var paperWin = (elements, element, choice) => element === paper && (elements[choice] === rock);
+      var scissorsLoss = (elements, element, choice) => element === scissors && (elements[choice] === rock);
+      var scissorsWin = (elements, element, choice) => element === scissors && (elements[choice] === paper);
       // scope shared functions
       $scope.onChoose = (element) => {
         $log.debug('on choose ' + element);
         var elements = [element];
         elements = nextElements(elements, element);
-        elemensts = randomReverse(elements);
+        elements = randomReverse(elements);
         $log.debug(elements);
         var machineChoose = machineChoice();
         $log.debug('machineChoose value ' + machineChoose);
@@ -137,6 +148,7 @@ if (Meteor.isClient) {
         }
 
         $scope.rounds += 1;
+        $scope.yourChoices.push(element);
       };
     }])
     .run([
